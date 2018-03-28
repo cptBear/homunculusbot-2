0ea5fc1e3b8eca18f916d45c6d5baabc7ddf0b8d
cptBear <jonathanrudd108@gmail.com>
2018-03-28 19:47:04 +0100

    unspeakable agony

--- mybot.js
+++ mybot.js
@@ -32,16 +32,37 @@ homunculus.on("message", message => {
     console.error(err);
   }
 });
+
+//nasty tech tree table
+sql.get(`SELECT * FROM discoveries WHERE userId ="${message.author.id}"`).then(row => {
+  if (!row) {
+    sql.run("INSERT INTO disoveries (userId, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  ) => {
+    sql.run("CREATE TABLE IF NOT EXISTS stats (userId TEXT, d1 INTEGER, d2 INTEGER , d3 INTEGER, d3 INTEGER, d4 INTEGER, d5 INTEGER, d6 INTEGER, d7 INTEGER, d8 INTEGER, d9 INTEGER, d10 INTEGER)").then(() => {
+    sql.run("INSERT INTO stats (userId, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  });
+  //nasty stat table
+sql.get(`SELECT * FROM stats WHERE userId ="${message.author.id}"`).then(row => {
+  if (!row) {
+    sql.run("INSERT INTO stats (userId, health, sanity, logic, conviction, fame, infamy, goldpieces, lifestyle) VALUES (? ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 10, 10, 1, 1, 1, 1, 20, 2]);
+  ) => {
+    sql.run("CREATE TABLE IF NOT EXISTS stats (userId TEXT, health INTEGER, sanity INTEGER , logic INTEGER, conviction INTEGER, fame INTEGER, infamy INTEGER, goldpieces INTEGER, lifestyle INTEGER,)").then(() => {
+    sql.run("INSERT INTO stats (userId, health, sanity, logic, conviction, fame, infamy, goldpieces, lifestyle) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 10, 10, 1, 1, 1, 1, 20, 2]);
+  });
+});
+});
+
+//nasty token table
 sql.get(`SELECT * FROM tokens WHERE userId ="${message.author.id}"`).then(row => {
     if (!row) {
-      sql.run("INSERT INTO tokens (userId, idle, lessons, bursary, alchemy, apparatus, research, reputation, discovery, goldpieces, fire, earth, water, wind, mercury, salt, sulfur, lead, tin, iron, gold, copper, silver, antimony, arsenic, bismuth, boron, magnesium, phosphorus, platinum, potassium, stone, sulfur, zinc, sal ammoniac, aqua fortis, aqua regia, aqua vitae, amalgam, cinnabar, vitriol, biomass, albedo, rubedo, nigredo, citrinitas) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]);
+      sql.run("INSERT INTO tokens (userId, idle, lessons, bursary, alchemy, apparatus,  fire, earth, water, wind, mercury, salt, sulfur, lead, tin, iron, gold, copper, silver, antimony, arsenic, bismuth, boron, magnesium, phosphorus, platinum, potassium, stone, sulfur, zinc, sal ammoniac, aqua fortis, aqua regia, aqua vitae, amalgam, cinnabar, vitriol, biomass, albedo, rubedo, nigredo, citrinitas) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]);
     } else {
       sql.run(`UPDATE tokens SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
     }
   }).catch(() => {
     console.error;
     sql.run("CREATE TABLE IF NOT EXISTS tokens (userId TEXT, idle INTEGER, lessons INTEGER, bursary INTEGER, alchemy INTEGER, apparatus INTEGER, research INTEGER, reputation INTEGER, discovery INTEGER, goldpieces INTEGER, fire INTEGER, earth INTEGER, water INTEGER, wind INTEGER, mercury INTEGER, salt INTEGER, sulfur INTEGER, lead INTEGER, tin INTEGER, iron INTEGER, gold INTEGER, copper INTEGER, silver INTEGER, antimony INTEGER, arsenic INTEGER, bismuth INTEGER, boron INTEGER, magnesium INTEGER, phosphorous INTEGER, platinum INTEGER, potassium INTEGER, stone INTEGER, sulfur INTEGER,  zinc INTEGER, sal ammoniac INTEGER, aqua fortis INTEGER, aqua regia INTEGER, aqua vitae INTEGER, amalgam INTEGER, cinnabar INTEGER, vitriol INTEGER,  biomass INTEGER, albedo INTEGER, rubedo INTEGER, nigredo INTEGER, citrinitas INTEGER)").then(() => {
-      sql.run("INSERT INTO tokens (userId, idle, lessons, bursary, alchemy, apparatus, research, reputation, discovery, goldpieces, fire, earth, water, wind, mercury, salt, sulfur, lead, tin, iron, gold, copper, silver, antimony, arsenic, bismuth, boron, magnesium, phosphorus, platinum, potassium, stone, sulfur, zinc, sal ammoniac, aqua fortis, aqua regia, aqua vitae, amalgam, cinnabar, vitriol, biomass, albedo, rubedo, nigredo, citrinitas) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]);
+      sql.run("INSERT INTO tokens (userId, idle, lessons, bursary, alchemy, apparatus, fire, earth, water, wind, mercury, salt, sulfur, lead, tin, iron, gold, copper, silver, antimony, arsenic, bismuth, boron, magnesium, phosphorus, platinum, potassium, stone, sulfur, zinc, sal ammoniac, aqua fortis, aqua regia, aqua vitae, amalgam, cinnabar, vitriol, biomass, albedo, rubedo, nigredo, citrinitas) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.author.id, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]);
     });
   });
 });

7f1561ae6a266a710fc15c3bd3f9830c98610407
cptBear <jonathanrudd108@gmail.com>
2018-03-28 15:49:18 +0100

    commit 3

--- mybot.js
+++ mybot.js
@@ -2,14 +2,14 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-
+client.on("ready", () => {
+  console.log("I am ready!");
+  message.channel.send("Your bidding, master?");
+});
 client.on("message", (message) => {
   if (message.content.startsWith("Good Homunculus!")) {
     message.channel.send("SQUEAL! Thankyou kind master! We tries.");
   }
 });
-client.on("ready", () => {
-  console.log("I am ready!");
-message.channel.send("Your bidding, master?");
-});
+
 client.login("NDI4NTQwMTcxNjUxMTIxMTUz.DZ0krQ.n3zuvHa1LY30RoUxlI5ePVhemxE");
